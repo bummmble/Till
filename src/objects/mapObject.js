aea0cfdf5cbd49d3fb99d12f5787c259ea8544a0
Forrest Collins <Forest.d.collins@gmail.com>
2017-12-11 12:08:00 -0600

    Cleans up mapObject a bit

--- src/objects/mapObject.js
+++ src/objects/mapObject.js
@@ -1,8 +1,6 @@
 export function mapObject(obj, fn) {
-    const props = Object.keys(obj);
-    const result = props.reduce((acc, curr) => {
+    return Object.keys(obj).reduce((acc, curr) => {
         acc[curr] = fn(obj[curr], curr, obj);
         return acc;
     }, {});
-    return result;
 }
